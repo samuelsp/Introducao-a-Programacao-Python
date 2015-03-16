e49945c6517c6c4548a3c3b4b8049cdf36aa2ea8
samuelsp <samp_81@yahoo.com.br>
2015-03-16 11:37:57 -0300

    ExibirTexto.py

---
+++ ExibirTexto.py
@@ -0,0 +1,25 @@
+##Exibindo Texto em Python
+print('Olá, Seja bem vindo ao meu programa em Python!')
+print("Vocẽ sabia que pode usar 'aspas simples' ou duplas para exibir strings?")
+print('''Voce pode inserir texto
+      que nao cabe em apenas uma linha.''')
+print("""Voce pode trabalhar com texto
+      que nao cabe apenas em uma linha.""")
+
+##Formataçoes em Python
+# \n -> insere uma nova linha
+print("Algum texto aqui\ne esse outro texto em outra linha.")
+
+# \t -> insere um espaço entre palavras
+print("Eu posso inserir um\tespaço entre palavras para formatar meu texto.")
+
+# + -> concatena strings
+print('Esse texto' + ' eu junto com esse outro texto.')
+print("Esse texto'" + "' eu junto novamente com esse outro texto.")
+print('Esse texto em aspas simples"' + " esse em aspas duplas.'")
+
+# \ -> caractere de escape
+print("Eu posso inserir uma palavra que contem \"aspas\" em meu texto.")
+print('Eu posso inserir uma palavra que contem \'aspas\' em meu texto.')
+print("C:\\Windows\\Documents\\file.txt")
+
